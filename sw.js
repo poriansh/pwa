96140ddaedd04e9f8b0b213fefe543bb733829a4
Pouria Neishabourian <pouria.neyshabourian@swai.ai>
2025-07-22 18:06:29 +0330

    test

--- sw.js
+++ sw.js
@@ -2,7 +2,11 @@
 self.addEventListener("install", () => {
   console.log("insta ser worker");
   self.skipWaiting(); // update automatically service worker
-  
+  caches.open("style cache").then((Cache) => {
+    Cache.add("style/index.css"); //  add chach file
+    Cache.add("/app.js"); //  add chach file
+    Cache.addAll(["style/index.css", "/app.js"]);
+  });
 });
 
 // activate service worker
@@ -11,6 +15,7 @@ self.addEventListener("activate", (event) => {
 });
 // fetch data source project
 self.addEventListener("fetch", (e) => {
-  e.respondWith(null); //  از کش بخون 
+  // e.respondWith(null);   از کش بخون
   e.respondWith(fetch(e.request)); // یا از شبکه بخون
+  e.respondWith
 });
