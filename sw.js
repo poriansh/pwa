a556af91b72d6025f7f63868a8471a7258e38507
poria neyshaboriyan <poria.nsh@gmail.com>
2025-07-22 21:21:45 +0330

    فثسف

--- sw.js
+++ sw.js
@@ -1,11 +1,12 @@
 // install service worker
-const cachversion = 8
+const cachversion = 10;
 const activeCach = {
-  assetscache : `cach-${cachversion}`
-} 
+  assetscache: `cach-${cachversion}`,
+};
 self.addEventListener("install", (e) => {
   self.skipWaiting(); // update automatically service worker
-  e.waitUntil( // اول کش بشه بعد بره به ایونت های دیگ 
+  e.waitUntil(
+    // اول کش بشه بعد بره به ایونت های دیگ
     caches.open(activeCach["assetscache"]).then((Cache) => {
       //Cache.add("style/index.css"); //  add chach file
       //Cache.add("/app.js"); //  add chach file
@@ -15,7 +16,21 @@ self.addEventListener("install", (e) => {
 });
 
 // activate service worker
-self.addEventListener("activate", (event) => {});
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    // پاک کردن کش های قدیمی 
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          // حذف همه کش‌ها به‌جز کش فعلی
+          if (cacheName !== activeCach.assetscache) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
 
 // fetch data source project
 self.addEventListener("fetch", (e) => {
