18132d4181f48ea09d9f06bad676e6abb8e9d008
Pouria Neishabourian <pouria.neyshabourian@swai.ai>
2025-09-04 23:22:21 +0330

    feat: refactor product fetching and enhance notification handling with service worker integration

--- js/code.js
+++ js/code.js
@@ -1,18 +1,23 @@
-const productContainer = document.getElementById('products');
-const notificationButton = document.getElementById('notification');
+const productContainer = document.getElementById("products");
+const notificationButton = document.getElementById("notification");
 async function fetchProducts() {
   try {
-    const res = await fetch('https://fakestoreapi.com/products');
+    const res = await fetch("https://fakestoreapi.com/products");
     const data = await res.json();
 
-    productContainer.innerHTML = data.slice(0, 5).map(product => `
+    productContainer.innerHTML = data
+      .slice(0, 5)
+      .map(
+        (product) => `
       <div class="card">
         <img src="${product.image}" alt="${product.title}" />
         <h2>${product.title}</h2>
         <p class="price">$${product.price}</p>
         <p class="rating">⭐ ${product.rating.rate} (${product.rating.count})</p>
       </div>
-    `).join('');
+    `
+      )
+      .join("");
   } catch (err) {
     console.error("Error fetching products:", err);
     productContainer.innerHTML = `<p>⚠️ Failed to load products.</p>`;
@@ -20,10 +25,45 @@ async function fetchProducts() {
 }
 fetchProducts();
 
+const showNotification = async () => {
+  if ("serviceWorker" in navigator) {
+    const sw = await navigator.serviceWorker.ready;
+    sw.showNotification(" sw دسترسی داده شد", {
+      body: "دسترسی داده شد",
+      dir: "rtl",
+      vibrate: [100, 50, 100],
+      icon: "assets/icon-192.png",
+      badge: "assets/icon-512.png",
+      image: "assets/icon-512.png",
+      tag: "sw test notification",
+      actions: [
+        {
+          action: "confirm",
+          title: "accept",
+        },
+        {
+          action: "reject",
+          title: "reject",
+        },
+      ],
+    });
+  }
+};
 
+const getPushSubscription = async () => {
+  if ("serviceWorker" in navigator) {
+    const serviceWorker = await navigator.serviceWorker.ready;
+    const subscription = await serviceWorker.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: "BGoKbJXGtVw927e14mB2tL8iJgI_Lp4_WxMj75QxU3",
+    });
+    console.log("subscription", subscription);
+  } else {
+  }
+};
 
-function getNotifications(){
-  // روش اول 
+function getNotifications() {
+  // روش اول
   // Notification.requestPermission().then((permission)=>{
   //   if(permission === "granted"){
   //     new Notification("دسترسی داده شد");
@@ -34,30 +74,11 @@ function getNotifications(){
   //   console.log("دسترسی داده شد");
   // }
   // روش سوم noti فقط در service worker باشه
-  Notification.requestPermission().then((permission)=>{
-   if("serviceWorker" in navigator){
-    navigator.serviceWorker.ready.then((sw)=>{
-      sw.showNotification(" sw دسترسی داده شد",{
-        body: "دسترسی داده شد",
-        dir: "rtl",
-        vibrate: [100, 50, 100],
-        icon: "assets/icon-192.png",
-        badge: "assets/icon-512.png",
-        image: "assets/icon-512.png",
-        tag: "sw test notification",
-        actions: [
-          {
-            action: "confirm",
-            title: "accept",
-          },
-          {
-            action: "reject",
-            title: "reject",
-          },
-        ]
-      });
-    });
-   }
+  Notification.requestPermission().then((permission) => {
+    if (permission === "granted") {
+      getPushSubscription();
+      showNotification();
+    }
   });
 }
 notificationButton.addEventListener("click", getNotifications);
