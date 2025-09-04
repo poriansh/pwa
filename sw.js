37edba2747c73db9bc11eabf29c9b813da7ff607
poria neyshaboriyan <poria.nsh@gmail.com>
2025-09-05 01:20:47 +0330

    fix: increment cache version in service worker from 8 to 9 for updated caching strategy

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@ const limitInCache = (key, size) => {
     });
   });
 };
-const cachversion = 8;
+const cachversion = 9;
 const activeCach = {
   static: `Static-${cachversion}`,
   dynamic: `dynamic-${cachversion}`,
